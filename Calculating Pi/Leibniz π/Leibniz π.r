9faf00d0a1055f54fc1cd3c7b8de720a439260ff
Ohnoimded <88839014+Ohnoimded@users.noreply.github.com>
2021-12-02 19:28:20 +0530

    Update Leibniz π.r

--- Calculating Pi/Leibniz π/Leibniz π.r
+++ Calculating Pi/Leibniz π/Leibniz π.r
@@ -1,8 +1,9 @@
-pi <- function(n) {
+# Using a variable input would be faster to find the decimals of pi for small number of digits
+pi <- function() { 
     sum <- 0
-    for (i in 0:n) {
+    for (i in 0:10e10) {
         sum <- sum + ((-1)^(i)) / ((2 * i)+1) #Leibniz pi/4 formula
     }
     return(4*sum)
 }
-print(formatC(pi(6e10), format = "f", digits = 25))
+print(formatC(pi(), format = "f", digits = 50))
